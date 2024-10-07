6d10f151baac12e3268320be99e2995e7699c560
Sayem Hossain <mail@sayem.dev>
2024-10-07 17:22:24 +0600

    Initial community commit

---
+++ convention-plugins/src/main/kotlin/module.publication.gradle.kts
@@ -0,0 +1,52 @@
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.kotlin.dsl.`maven-publish`
+
+plugins {
+    `maven-publish`
+    signing
+}
+
+publishing {
+    // Configure all publications
+    publications.withType<MavenPublication> {
+        // Stub javadoc.jar artifact
+        artifact(tasks.register("${name}JavadocJar", Jar::class) {
+            archiveClassifier.set("javadoc")
+            archiveAppendix.set(this@withType.name)
+        })
+
+        // Provide artifacts information required by Maven Central
+        pom {
+            name.set("Client Library")
+            artifactId = "clientlib"
+            description.set("Cognito platform client library")
+            url.set("https://cognitox.org")
+
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set("sayemoid")
+                    name.set("Sayem Hossain")
+                    organization.set("CognitoX")
+                    organizationUrl.set("https://cognitox.org")
+                }
+            }
+            scm {
+                url.set("https://github.com/Kotlin/multiplatform-library-template")
+            }
+        }
+    }
+}
+
+signing {
+    if (project.hasProperty("signing.gnupg.keyName")) {
+        useGpgCmd()
+        sign(publishing.publications)
+    }
+}
